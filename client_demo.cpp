701d52881f7131b0922721aa46b3c935e33ee2c1
Rob McColl <rob@robmccoll.com>
2013-05-01 00:33:29 -0400

    basic csv->client->server test working

--- client_demo.cpp
+++ client_demo.cpp
@@ -7,11 +7,13 @@ extern "C" {
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string>
+#include <netdb.h>
 
 using namespace gt::stinger;
 
@@ -36,8 +38,9 @@ main(int argc, char *argv[])
   int dst_port = 10101;
   uint64_t buffer_size = 1ULL << 28ULL;
   int use_strings = 0;
-  struct sockaddr_in server_addr;
-  struct hostent * server;
+  struct sockaddr_in server_addr = { 0 };
+  struct hostent * server = NULL;
+  char * filename;
 
   int opt = 0;
   while(-1 != (opt = getopt(argc, argv, "p:b:d:a:s"))) {
@@ -55,9 +58,11 @@ main(int argc, char *argv[])
       } break;
 
       case 'a': {
-	/* TODO
 	server = gethostbyname(optarg);
-	*/
+	if(NULL == server) {
+	  E_A("ERROR: server %s could not be resolved.", optarg);
+	  exit(-1);
+	}
       } break;
 
       case 's': {
@@ -67,9 +72,9 @@ main(int argc, char *argv[])
       case '?':
       case 'h': {
 	printf("Usage:    %s [-p src_port] [-d dst_port] [-a server_addr] [-b buffer_size] [-s for strings] <input_csv>\n", argv[0]);
-	printf("Defaults: src_port: %d buffer_size: %lu use_strings: %d\n", src_port, (unsigned long) buffer_size, use_strings);
-	printf("\nCSV file format is is_delete,source,dest,weight,time where weight and time are"
-		 "optional 64-bit integers and source and dest are either strings or"
+	printf("Defaults: src_port: %d dest_port: %d server: localhost buffer_size: %lu use_strings: %d\n", src_port, dst_port, (unsigned long) buffer_size, use_strings);
+	printf("\nCSV file format is is_delete,source,dest,weight,time where weight and time are\n"
+		 "optional 64-bit integers and source and dest are either strings or\n"
 		 "64-bit integers depending on options flags. is_delete should be 0 for edge\n"
 		 "insertions and 1 for edge deletions.\n");
 	exit(0);
@@ -80,9 +85,23 @@ main(int argc, char *argv[])
   if(optind >= argc) {
     perror("Expected input CSV filename. -? for help");
     exit(0);
+  } else {
+    filename = argv[optind];
   }
 
   V_A("Running with: src_port: %d dst_port: %d buffer_size: %lu string-vertices: %d\n", src_port, dst_port, (unsigned long) buffer_size, use_strings);
+
+  if(NULL == server) {
+    server = gethostbyname("localhost");
+    if(NULL == server) {
+      E_A("ERROR: server %s could not be resolved.", "localhost");
+      exit(-1);
+    }
+  }
+
+  server_addr.sin_family = AF_INET;
+  memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+  server_addr.sin_port = dst_port;
 
   int sock_handle;
   struct sockaddr_in sock_addr = { AF_INET, (in_port_t)src_port, 0};
@@ -92,15 +111,15 @@ main(int argc, char *argv[])
     exit(-1);
   }
 
-  if(-1 == bind(sock_handle, (const sockaddr *)&sock_addr, sizeof(sock_addr))) {
+  /*if(-1 == bind(sock_handle, (const sockaddr *)&sock_addr, sizeof(sock_addr))) {
     perror("Socket bind failed");
     exit(-1);
-  }
+  }*/
 
-  if(-1 == listen(sock_handle, 1)) {
+  /*if(-1 == listen(sock_handle, 1)) {
     perror("Socket listen failed");
     exit(-1);
-  }
+  }*/
 
   uint8_t * buffer = (uint8_t *)malloc(buffer_size);
   if(!buffer) {
@@ -108,11 +127,16 @@ main(int argc, char *argv[])
     exit(-1);
   }
 
+  if(-1 == connect(sock_handle, (const sockaddr *)&server_addr, sizeof(server_addr))) {
+    perror("Connection failed");
+    exit(-1);
+  }
+
   V("Parsing messages.");
 
-  FILE * fp;
-  char * buf, ** fields;
-  uint64_t bufSize, * lengths, fieldsSize, count;
+  FILE * fp = fopen(filename, "r");
+  char * buf = NULL, ** fields = NULL;
+  uint64_t bufSize = 0, * lengths = NULL, fieldsSize = 0, count = 0;
   int64_t line = 0;
 
   StingerBatch batch;
@@ -122,6 +146,8 @@ main(int argc, char *argv[])
     line++;
     readCSVLineDynamic(',', fp, &buf, &bufSize, &fields, &lengths, &fieldsSize, &count);
 
+    if(count <= 1)
+      continue;
     if(count < 3) {
       E_A("ERROR: too few elements on line %ld", (long) line);
       continue;
@@ -165,6 +191,8 @@ main(int argc, char *argv[])
 
   batch.SerializeToString(&out_buffer);
 
+  write(sock_handle, out_buffer.c_str(), out_buffer.size());
 
+  free(buffer);
   return 0;
 }
